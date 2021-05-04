2e32aa1b51826e2b1113c4584d8535dfcf3bb494
Robert Barisic <xrlabs@googlemail.com>
2021-05-04 18:56:41 +0200

    check env

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AuthController < ApplicationController
   before_action :authenticate_user!, unless: -> { request.format == :json }
-  before_action :check_current_user, only: %i[ current show update destroy authenticated ]
+  # before_action :check_current_user, only: %i[ current show update destroy authenticated ]
 
   def authenticated
     render inline: {authorized: true}.to_json
